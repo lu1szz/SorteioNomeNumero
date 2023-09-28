2b0404afcf9e347c5d39c4d51174bd9e1885ac5b
lu1szz <133365448+lu1szz@users.noreply.github.com>
2023-09-28 10:23:16 -0300

    Add files via upload

---
+++ sorteio.js
@@ -0,0 +1,9 @@
+// Função para redirecionar para a página de sorteio de números
+document.getElementById('sorteio-numero').addEventListener('click', function() {
+    window.location.href = 'index.html';
+});
+
+// Função para redirecionar para a página de sorteio de nomes
+document.getElementById('sorteio-nome').addEventListener('click', function() {
+    window.location.href = 'nome.html';
+});
